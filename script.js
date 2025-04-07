214d6ca31774890ef3afce4b38847c0a2b364df4
gabrielsilva213 <145125646+gabrielsilva213@users.noreply.github.com>
2025-04-07 11:06:35 -0300

    alto-contraste

--- script.js
+++ script.js
@@ -24,6 +24,8 @@ document.addEventListener('DOMContentLoaded', function () {
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    
 
     })
+
 })
